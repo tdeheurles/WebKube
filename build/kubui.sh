baf7e21107995510711c200b13f6306156c64b54
tdeheurles <tdeheurles@gmail.com>
2015-06-16 11:26:38 +0200

    Added volume for ivy and sbt cache

--- build/kubui.sh
+++ build/kubui.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+echo " "
+echo "From jvm-tools :"
+echo "USERNAME"
+username
+echo " "
+
 cd /workspace
 ./activator clean stage
--- build/main.sh
+++ build/main.sh
@@ -4,6 +4,16 @@
 # We share Epsilon-Bootstrap for maven container acces
 # we share .m2 in jenkins-agent for settings
 # import names
+
+echo " "
+echo "USERNAME"
+username
+echo " "
+echo "gcloud context"
+gcloud config list
+
+
+
 . ./build/release.cfg
 artifactname="gcr.io/$projectid/$servicename:$servicemajor.$serviceminor.$BUILD_NUMBER"
 echo "artifactname=\"$artifactname\"" > ./build/containername.cfg
@@ -14,7 +24,9 @@ workspace="workspace"
 docker run \
   --rm \
   -v "$(pwd):/$workspace" \
-  gcr.io/adaptive-jenkins/jvm-tools:latest "/$workspace/build/kubui.sh"
+  -v "/home/$(username)/.ivy2:/home/builder/.ivy2"
+  -v "/home/$(username)/.sbt:/home/builder/.sbt"
+  gcr.io/adaptive-jenkins/jvm-tools:0.0.3 "/$workspace/build/kubui.sh"
 
 
 # Prepare container
