293c7c87dbb260635d7a5cb71cc4c5346d873eb6
tdeheurles <tdeheurles@gmail.com>
2015-06-16 09:40:54 +0200

    Initialisation build

---
+++ build/main.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Jenkins Entrypoint
+
+# We share Epsilon-Bootstrap for maven container acces
+# we share .m2 in jenkins-agent for settings
+# import names
+. ./build/release.cfg
+artifactname="gcr.io/$projectid/$servicename:$servicemajor.$serviceminor.$BUILD_NUMBER"
+echo "artifactname=\"$artifactname\"" > ./build/containername.cfg
+
+
+# Build binaries
+workspace="workspace"
+docker run \
+  --rm \
+  -v "$(pwd):/$workspace" \
+  tdeheurles/jvm-tools "/$workspace/build/kubui.sh"
+
+
+# Prepare container
+cp  ./target/
+docker build -t $artifactname ./build/container/
+
+
+# Push to Google Cloud Engine
+# gcloud preview docker push $artifactname
