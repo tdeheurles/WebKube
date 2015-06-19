b9bc6801d0134d81679289eb966f612dae991520
tdeheurles <tdeheurles@gmail.com>
2015-06-19 17:57:55 +0200

    Change to manifets names

--- build/generate_manifests.sh
+++ build/generate_manifests.sh
@@ -10,7 +10,7 @@ versiontag=$servicemajor.$serviceminor.$BUILD_NUMBER
 
 # RC
 rcfile=./deploy/kubernetes/$servicename\_$versiontag\_rc.json
-rcname=$servicename-m$servicemajor\m$serviceminor\m$BUILD_NUMBER
+rcname=$servicename
 
 sed "s/__rcName__/$rcname/g" \
     ./deploy/kubernetes/$k8s_api_version/rc.template.json > $rcfile
@@ -26,12 +26,13 @@ sed -i "s/__privatePort__/$serviceport/g" $rcfile
 # SERVICE
 servicefile=./deploy/kubernetes/$servicename\_$versiontag\_service.json
 
-sed "s/__serviceName__/$servicename-m$servicemajor/g" \
+sed "s/__serviceName__/$servicename/g" \
     ./deploy/kubernetes/$k8s_api_version/service.template.json > $servicefile
 sed -i "s/__major__/$servicemajor/g" $servicefile
 sed -i "s/__minor__/$serviceminor/g" $servicefile
 sed -i "s/__build__/$BUILD_NUMBER/g" $servicefile
 sed -i "s/__image__/$containername/g" $servicefile
 sed -i "s/__privatePortName__/$servicename/g" $servicefile
-sed -i "s/__privatePort__/$serviceport/g" $servicefile
+sed -i "s/__publicPortName__/$servicename/g" $servicefile
+sed -i "s/__publicPort__/$serviceport/g" $servicefile
 sed -i "s/__rcName__/$rcname/g" $servicefile
